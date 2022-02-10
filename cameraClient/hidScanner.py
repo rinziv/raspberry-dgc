3c9f02d3d75a6b894101662ec85d2cde9423b4c8
rinziv <rinzivillo@isti.cnr.it>
2022-02-10 20:58:34 +0100

    adding color for status of qr code

--- cameraClient/hidScanner.py
+++ cameraClient/hidScanner.py
@@ -1,11 +1,70 @@
 import evdev
 import requests
 from evdev import *
+import RPi.GPIO as GPIO
 import time 
+
+
+colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF]
+R = 11
+G = 12
+B = 13
+
+def setup(Rpin, Gpin, Bpin):
+   global pins
+   global p_R, p_G, p_B
+   pins = {'pin_R': Rpin, 'pin_G': Gpin, 'pin_B': Bpin}
+   GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+   for i in pins:
+      GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
+      GPIO.output(pins[i], GPIO.HIGH) # Set pins to high(+3.3V) to off led
+
+   p_R = GPIO.PWM(pins['pin_R'], 2000)  # set Frequece to 2KHz
+   p_G = GPIO.PWM(pins['pin_G'], 1999)
+   p_B = GPIO.PWM(pins['pin_B'], 5000)
+
+   p_R.start(100)      # Initial duty Cycle = 0(leds off)
+   p_G.start(100)
+   p_B.start(100)
+
+def map(x, in_min, in_max, out_min, out_max):
+   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def off():
+   GPIO.setmode(GPIO.BOARD)
+   for i in pins:
+      GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
+      GPIO.output(pins[i], GPIO.HIGH)    # Turn off all leds
+
+def setColor(col):   # For example : col = 0x112233
+   R_val = (col & 0xff0000) >> 16
+   G_val = (col & 0x00ff00) >> 8
+   B_val = (col & 0x0000ff) >> 0
+
+   R_val = map(R_val, 0, 255, 0, 100)
+   G_val = map(G_val, 0, 255, 0, 100)
+   B_val = map(B_val, 0, 255, 0, 100)
+
+   p_R.ChangeDutyCycle(100-R_val)     # Change duty cycle
+   p_G.ChangeDutyCycle(100-G_val)
+   p_B.ChangeDutyCycle(100-B_val)
+
+
+
+
+## Starting configuration
+setup(R, G, B)
+off()
+
+
 dev =evdev.InputDevice('/dev/input/by-id/usb-SM_SM-2D_PRODUCT_HID_KBW_APP-000000000-event-kbd')
 #dev =evdev.InputDevice('/dev/input/event4')
 dev.grab()
 print(dev)
+
+setColor(0x333333) #bianco
+
+
 
 # for event in dev.read_loop():
 #     if event.type == ecodes.EV_KEY:
@@ -59,13 +118,25 @@ def read_scan():
        yield x 
        x = ''
 
+def destroy():
+   p_R.stop()
+   p_G.stop()
+   p_B.stop()
+   off()
+   GPIO.cleanup()
 
 
 code_generator = read_scan()
-for i in code_generator:
-  print('code', i)
-  payload = {'dgc': i}
-  r = requests.get('http://localhost:3000/', params=payload)
-  print('Return code: ', r.status_code, ', Text: ', r.text)
-
-
+try:
+  for i in code_generator:
+    setColor(0x0000FF)
+    print('code', i)
+    payload = {'dgc': i}
+    r = requests.get('http://localhost:3000/', params=payload)
+    print('Return code: ', r.status_code, ', Text: ', r.text)
+    if r.status_code == 200:
+       setColor(0xFF00FF)
+    else:
+       setColor(0x00FFFF)
+except KeyboardInterrupt:
+    destroy()
